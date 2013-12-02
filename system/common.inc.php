d62f47b4a048faefaefe3341663f8cb3f5673471
kookxiang <i@ikk.me>
2013-12-02 13:14:35 +0800

    移除废弃代码

--- system/common.inc.php
+++ system/common.inc.php
@@ -15,9 +15,6 @@ if($_GET['debug']){
 }
 require_once SYSTEM_ROOT.'./class/core.php';
 $system = new kk_sign();
-if($uid && SYS_KEY && getSetting('SYS_KEY') != SYS_KEY){
-	saveSetting('SYS_KEY', SYS_KEY);
-}
 $formhash = substr(md5(substr(TIMESTAMP, 0, -7).$username.$uid.ENCRYPT_KEY.ROOT), 8, 8);
 $sitepath = substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/'));
 $siteurl = htmlspecialchars('http://'.$_SERVER['HTTP_HOST'].$sitepath.'/');
