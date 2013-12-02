a17270b627cd0fb2d9940c6615ad1eb5078176ba
kookxiang <i@ikk.me>
2013-12-02 13:02:14 +0800

    移除1.13.8.21及以下版本的升级支持

--- system/function/updater/fallback.php
+++ system/function/updater/fallback.php
@@ -8,10 +8,6 @@ while($table = DB::fetch($query)){
 
 if(!in_array('member', $tables)){
 	include SYSTEM_ROOT.'./function/updater/install.php';
-}elseif(!in_array('setting', $tables)){
-	include SYSTEM_ROOT.'./function/updater/1.0.php';
-}elseif(!in_array('member_setting', $tables)){
-	include SYSTEM_ROOT.'./function/updater/1.13.8.18.php';
 }
 throw new Exception('找不到更新程序，无法进行更新！');
 ?>
